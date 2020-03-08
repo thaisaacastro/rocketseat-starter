308fc64d302e9202d0a1a1a4f44c2231c0ff385b
thaisaacastro <thaisascastro@gmail.com>
2020-03-08 20:30:03 -0300

    Conexao com banco de dados -Mongodb-

---
+++ node-api/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require('mongoose');
+
+//Iniciando o App
+const app = express();
+
+//Iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+//Primeira rota
+app.get("/", (req, res) => {
+  res.send("Hello Guy");
+});
+
+app.listen(3001);
