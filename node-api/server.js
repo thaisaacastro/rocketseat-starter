ae19467f343c81093d0ceaf1de92736e682070fc
thaisaacastro <thaisascastro@gmail.com>
2020-03-08 21:11:15 -0300

    Adicionando Produto no Banco de Dados

--- node-api/server.js
+++ node-api/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 //Iniciando o App
 const app = express();
@@ -10,9 +11,19 @@ mongoose.connect('mongodb://localhost:27017/nodeapi', {
   useUnifiedTopology: true,
 });
 
+requireDir("./src/models");
+
+const Product = mongoose.model('Product');
+
+
 //Primeira rota
 app.get("/", (req, res) => {
-  res.send("Hello Guy");
+  Product.create({
+    title: 'React Native',
+    description: 'Build native apps with React',
+    url: 'http://github.com/facebook/react-native'
+  })
+  return res.send("Hello Guy");
 });
 
 app.listen(3001);
