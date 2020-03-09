6f3bb14ea48183d17c32e2a1b00e92164fc2d52e
thaisaacastro <thaisascastro@gmail.com>
2020-03-08 21:44:37 -0300

    Adicionando camada de controller e retornando dados do banco

--- node-api/server.js
+++ node-api/server.js
@@ -13,17 +13,8 @@ mongoose.connect('mongodb://localhost:27017/nodeapi', {
 
 requireDir("./src/models");
 
-const Product = mongoose.model('Product');
 
+//Rotas
 
-//Primeira rota
-app.get("/", (req, res) => {
-  Product.create({
-    title: 'React Native',
-    description: 'Build native apps with React',
-    url: 'http://github.com/facebook/react-native'
-  })
-  return res.send("Hello Guy");
-});
-
+app.use('/api', require('./src/routes'));
 app.listen(3001);
