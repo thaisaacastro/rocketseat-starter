19432a6f1a75846a8781f339cb2606b3cae1f00b
thaisaacastro <thaisascastro@gmail.com>
2020-03-10 22:52:16 -0300

    produtos da API

--- huntweb/src/App.js
+++ huntweb/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+
 import "./style.css"
+
 import Header from './componentes/Header';
+import Main from './pages/main';
 
 const App = () => (
   <div className="App">
     <Header />
+    <Main />
   </div>
 );
 
